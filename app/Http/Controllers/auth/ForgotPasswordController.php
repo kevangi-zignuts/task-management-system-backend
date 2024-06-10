baadc96e77b8601ceee676f98c7a45d6f14c31f4
kevangi-zignuts <kevangip@zignuts.com>
2024-06-10 12:14:36 +0530

    Add a forgot password api and add a mail for sending a reset password link

---
+++ app/Http/Controllers/auth/ForgotPasswordController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use App\Mail\ForgetPassword;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
+
+class ForgotPasswordController extends Controller
+{
+    public function forgotPassword(Request $request){
+        $request->validate([
+            'email' => 'required|email|exists:users',
+        ]);
+        $user  = User::where('email', $request->email)->first();
+        $token = Str::random(64);
+        // Update or insert the password reset token for the user
+        DB::table('password_reset_tokens')->updateOrInsert(
+        [
+            'email' => $request->email,
+        ],
+        [
+            'token' => $token,
+            'created_at' => Carbon::now(),
+        ]);
+
+        $resetLink = env('VUE_APP_URL'). '/reset-password/'. $token;
+
+        // Send a password reset email to the user
+        Mail::to($user->email)->send(new ForgetPassword($resetLink ,$token, $user->first_name));
+
+        return response()->json([
+            'token'   => $token
+        ], 200);
+    }
+}
---
+++ resources/views/emails/forgetPassword.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Forgot Password - mail</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+        .card {
+            width: 75%
+        }
+
+        @media screen and (width <=600px) {
+            .card {
+                width: 100%;
+            }
+        }
+    </style>
+</head>
+
+<body class="bg-light">
+    <div class="container mx-auto mt-5">
+        <div class="card mx-auto mx-100 p-3 shadow-lg mb-5 bg-white rounded">
+            <div class="card-body text-left">
+                <p class="heading">hello {{ $name }}!, </p>
+                <p class="mb-4 mt-3">Someone requested a link to change your password. Click the button below to proceed
+                </p>
+                <a href="{{ $resetLink }}" class="btn btn-primary text-center">Change
+                    my password</a>
+                <p class="mb-4 mt-3">If you didn't request this, please ignore the email. Your password will stay safe
+                    and won't be changed.</p>
+                <p class="border-top pt-2">Sincerely,</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
