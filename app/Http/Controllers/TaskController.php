9ae238ab3aeefdd1c5310a3d905a8e88bf6513df
kevangi-zignuts <kevangip@zignuts.com>
2024-06-10 12:10:28 +0530

    add create, edit, view and delete task api

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,10 +17,76 @@ class TaskController extends Controller
     *   @return \Illuminate\Http\Response
     */
     public function index() {
-        $tasks=Task::all();
+        $tasks=Task::where('user_id', auth()->user()->id)->get();
        
         return response()->json($tasks, 200);
     }
 
-   
+    /**
+    *   This function is used to Store task Details
+    *   @method POST
+    *   @route  /tasks/store (protected)
+    *   @middleware (auth:sanctum)
+    *   @param \Illuminate\Http\Request $request
+    *   @return \Illuminate\Http\Response
+    */
+    public function store(Request $request){
+        $task = $request->validate([
+            'name' => 'required|string|max:64',
+            'due_date' => 'required|date',
+            'description' => 'nullable|string|max:255',
+        ]);
+        Task::create(array_merge($task, ['user_id' => Auth::id()]));
+        return response()->json(['task' => $task], 201);
+    }
+
+    /**
+    *   This function is used to Update task Details
+    *   @method POST
+    *   @route  /tasks/update/{id} (protected)
+    *   @middleware (auth:sanctum)
+    *   @param \Illuminate\Http\Request $request, id
+    *   @return \Illuminate\Http\Response
+    */
+    public function update(Request $request, $id){
+        $task = $request->validate([
+            'name' => 'required|string|max:64',
+            'due_date' => 'required|date',
+            'description' => 'nullable|string|max:255',
+        ]);
+        $task = Task::findOrFail($id);
+        $task->update($request->only(['name', 'due_date', 'description']));
+
+        return response()->json(['task' => $task], 200);
+    }
+
+    /**
+    *   This function is used to View task Details
+    *   @method Get
+    *   @route  /tasks/view/{id} (protected)
+    *   @middleware (auth:sanctum)
+    *   @param \Illuminate\Http\Request $request, id
+    *   @return \Illuminate\Http\Response
+    */
+    public function view($id){
+        $task = Task::findOrFail($id);
+        return response()->json(['task' => $task], 200);
+    }
+
+    /**
+    *   This function is used to Delete task Details
+    *   @method Get
+    *   @route  /tasks/view/{id} (protected)
+    *   @middleware (auth:sanctum)
+    *   @param \Illuminate\Http\Request $request, id
+    *   @return \Illuminate\Http\Response
+    */
+    public function delete($id){
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return response()->json([
+            'status'  => 'success',
+            'message' => "Task's data deleted Successfully",
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\auth\LoginController;
+use App\Http\Controllers\auth\RegistrationController;
 
 Route::post('/', [LoginController::class, 'login']);
 Route::post('/register', [RegistrationController::class, 'register']);
 Route::get('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::get('/tasks', [TaskController::class, 'index']);
+// Routes for tasks crud operation
+Route::middleware(['auth:sanctum'])->prefix('tasks')->controller(TaskController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::post('/store', 'store');
+    Route::post('/update/{id}', 'update');
+    Route::get('/view/{id}', 'view');
+    Route::get('/delete/{id}', 'delete');
+});
